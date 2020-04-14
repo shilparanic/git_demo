f3714a46c9dc3383da454450f81bb70e2b5f35d3
shilparanic <shilparanic16@gmail.com>
2020-04-13 23:19:32 -0700

    this is sri login page

--- Git_demo.java
+++ Git_demo.java
@@ -5,6 +5,8 @@ public class Git_demo {
 	public static void main(String[] args) {
 		
 System.out.println("Learning git today");
+System.out.println("master branch");
+System.out.println("i am making changes in rusheek branch");
 	}
 
 }
